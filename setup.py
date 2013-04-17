7a598e9c5dd39d9ad5775f24cbe9fe330071a0fb
timknip <tim@floorplanner.com>
2013-04-17 02:34:49 +0200

    setting version to 0.1

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.  
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='pycsg',
+    version='0.1',
+    description='Constructive Solid Geometry (CSG)',
+    long_description=read('README.md'),
+    keywords = "constructive solid geometry csg utilities",
+
+    author='Tim Knip',
+    author_email='tim@floorplanner.com',
+    url='https://github.com/timknip/pycsg',
+
+    install_requires = [],
+    package_dir={'csg': 'src'},
+    packages=['csg'],
+    license = "MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
