d5ee93056c712e772a6994a2ebe5d66e8c1a22fd
Tim Knip <tim@floorplanner.com>
2015-07-24 19:43:29 +0200

    bumping to 0.3.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-# Utility function to read the README file.  
+# Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='pycsg',
-    version='0.2',
+    version='0.3.0',
     description='Constructive Solid Geometry (CSG)',
     long_description=read('README.md'),
     keywords = "constructive solid geometry csg utilities",
